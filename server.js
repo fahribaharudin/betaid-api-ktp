2bbecaf8ffaa351f82d106b44caf8a272be2540b
Fahri Baharudin <fahrybaharudin@gmail.com>
2015-10-09 18:45:09 +0700

    upgrading the api

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    _ = require('underscore'),
     app = express();
 
 var id = 7;
-var data = {
-    1: { 
+var data = [
+    { 
         NIK: 12001, 
         nama: 'Fahri Baharudin', 
         ttl: 'Wonosobo, 16-11-1993', 
@@ -17,7 +18,7 @@ var data = {
         masaBerlaku: '16-11-2017',
         namaIbu: 'Jamilah'
     },
-    2: { 
+    { 
         NIK: 12002, 
         nama: 'Dwi Hardiman', 
         ttl: 'Wonosobo, 21-09-1994', 
@@ -30,7 +31,7 @@ var data = {
         masaBerlaku: '21-09-2017',
         namaIbu: 'Tumini'
     },
-    3: { 
+    { 
         NIK: 12003, 
         nama: 'Maulana Nur Aji', 
         ttl: 'Wonosobo, 10-10-1993', 
@@ -43,18 +44,27 @@ var data = {
         masaBerlaku: '10-10-2017',
         namaIbu: 'Siam'
     },
-};
+];
 
 app.use(bodyParser.json());
 app.use(express.static('./public'));
 
 app.route('/ktp')
     .get(function(req, res) {
-        var ktp = {}; 
-        ktp.data = Object.keys(data).map(function(key) {
-            return data[key];
-        });
-        res.json(ktp);
+        res.json({ data: data, status: 200 });
+    });
+
+app.route('/ktp/:id')
+    .get(function(req, res) {
+        var person = _.find(data, function(item) {
+            return item.NIK == req.params.id;
+        }); 
+
+        if (person !== undefined) {
+            res.json({ data: person, status: 200 });
+        } else {
+            res.status(404).json({ error: { message: 'Data tidak ditemukan' }, status: 404 });
+        }
     });
 
 app.route('/')
@@ -62,5 +72,7 @@ app.route('/')
         res.redirect('/ktp');
     });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function() {
+    console.log('server running!');
+});
 
